85e5ea8d6b65cafa5e5fd19b72ef9141d79b4d9f
t2506nzd <t2506nzd@prestadorcbmp.com.br>
2020-10-22 18:30:53 -0300

    sqflite

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,22 +1,49 @@
+import 'package:bank_flutter/database/app_database.dart';
 import 'package:bank_flutter/models/contact.dart';
 import 'package:bank_flutter/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ListaDeContatos extends StatelessWidget {
-  final List<Contato> contacts = List();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de Usuário'),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Contato contact = contacts[index];
-          return _ContactItem(contact);
+      body: FutureBuilder<List<Contato>>(
+        initialData: List(),
+        future: findAll(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircularProgressIndicator(),
+                    Text('loading...'),
+                  ],
+                ),
+              );
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contato> contacts = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contato contact = contacts[index];
+                  return _ContactItem(contact);
+                },
+                itemCount: contacts.length,
+              );
+              break;
+          }
+          return Text('unknown error');
         },
-        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
